99aa62a9e93a12356b061757b31071e415f8b802
djin1430 <djinmendoza@gmail.com>
2021-10-06 01:07:31 +0800

    Added Models to Admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Amenities, Services, OperationHours, Comments, Barbershop, Profile
 
 # Register your models here.
+admin.site.register(Amenities)
+admin.site.register(Services)
+admin.site.register(OperationHours)
+admin.site.register(Comments)
+admin.site.register(Barbershop)
+admin.site.register(Profile)

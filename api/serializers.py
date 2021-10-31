16946c8b8db83aeaca29dcb0314ee238e62db340
Daniel Soriano <sorianodanielb99@gmail.com>
2021-10-31 16:01:04 +0800

    Update BarbershopProfile Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -179,6 +179,7 @@ class BarbershopSerializer(serializers.ModelSerializer):
 
 
 class BarbershopProfileSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
     class Meta:
         model = Barbershop
         fields = [
